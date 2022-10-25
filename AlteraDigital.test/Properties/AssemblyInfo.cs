a81e80f769b4b10acdeab52d8b29fc9d814d0582
Vinith Ramedi <Vinith.Ramedi@allscripts.com>
2022-10-26 01:42:53 +0530

    Update

---
+++ AlteraDigital.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AlteraDigital.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlteraDigital.test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c156203-a76d-4489-a236-d43f61d2c1ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AlteraDigital.test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using AlteraDigitalBarberShop.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AlteraDigital.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetItemTest()
+        {
+            var controller = new FacialController();
+
+            var GetItem_result = controller.Facial();
+            //Assert    
+            Assert.IsNotNull(GetItem_result);
+        }
+        [TestMethod]
+        public void GetTest()
+        {
+            var controller = new StyleController();
+
+            var Get_result = controller.Styles();
+            //Assert    
+            Assert.IsNotNull(Get_result);
+        }
+    }
+}
+
